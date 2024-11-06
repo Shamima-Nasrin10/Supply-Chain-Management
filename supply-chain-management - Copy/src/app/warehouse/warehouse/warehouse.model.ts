b3fe93f023525102517447a0f3d912462465fcdb
Shamima-Nasrin10 <72626163+Shamima-Nasrin10@users.noreply.github.com>
2024-11-06 14:02:51 +0600

    Warehouse

--- supply-chain-management - Copy/src/app/warehouse/warehouse/warehouse.model.ts
+++ supply-chain-management - Copy/src/app/warehouse/warehouse/warehouse.model.ts
@@ -3,4 +3,11 @@ export class WareHouse{
   id!: number;
   name!: string;
   location!: string;
+  status !: ApprovalStatus;
+}
+
+export enum ApprovalStatus {
+  PENDING = 'PENDING',
+  APPROVED = 'APPROVED',
+  REJECTED = 'REJECTED'
 }
