f0d6f52a2cb009d9ab5456f9ba7a392ccc3b19a7
Shamima-Nasrin10 <72626163+Shamima-Nasrin10@users.noreply.github.com>
2024-11-27 15:35:04 +0600

    Security config

--- SCMSystem_SpringBoot/src/main/java/com/shamima/SCMSystem/security/configuration/SecurityConfig.java
+++ SCMSystem_SpringBoot/src/main/java/com/shamima/SCMSystem/security/configuration/SecurityConfig.java
@@ -77,13 +77,13 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200", "10.0.2.2:8804"));  // Add allowed origins
+        configuration.setAllowedOrigins(List.of("http://localhost:4200", "10.0.2.2:8804", "http://localhost:8804"));  // Add allowed origins
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
         configuration.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);  // Apply CORS settings to all endpoints
+        source.registerCorsConfiguration("/**", configuration);  
         return source;
     }
 
